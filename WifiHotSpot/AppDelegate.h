f08454b5883d00edee29dad36c4b35d80c4b05f9
Olusesan <olusesan.ajina@gmail.com>
2015-03-25 12:01:45 -0400

    Initial Commit

---
+++ WifiHotSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WifiHotSpot
+//
+//  Created by Olusesan Ajina on 3/25/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WifiHotSpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WifiHotSpot
+//
+//  Created by Olusesan Ajina on 3/25/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
